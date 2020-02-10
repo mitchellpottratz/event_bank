25143adf079a485f1a92aea876a757d8b826c16c
Mitchell Pottratz <mitchell.pottratz@yahoo.com>
2020-02-09 18:40:39 -0600

    refactored App into a class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 
-function App() {
-  return (
-    <div>
-    
-    </div>
-  );
-}
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
 
+}
 
 export default App;
